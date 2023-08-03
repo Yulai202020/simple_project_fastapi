84bfa800e332c9d4628d927804295c480efd6c84
Yulai202020 <ngmyulai@gmail.com>
2023-08-03 22:13:12 +0500

    first commit

---
+++ router.py
@@ -0,0 +1,30 @@
+import os
+from fastapi import APIRouter
+from fastapi import APIRouter
+
+table_name: str = os.environ.get("TABLE_NAME")
+
+persons = {
+    "0": [
+        {"name": "Yulai"},
+        {"surname": "Nigmatullin"}
+    ],
+    "1": [
+        {"name": "Azamat"},
+        {"surname": "Nigmatullin"}
+    ]
+}
+
+# Create api router
+router = APIRouter()
+
+# Get all Persons
+@router.get("/")
+async def home_all():
+    return persons
+
+# Get person by ID
+@router.get("/persons/{person_id}")
+async def get_person(person_id: int):
+    _person = str(person_id)
+    return persons[_person]
