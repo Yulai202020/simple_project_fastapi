dba6dccf3b1ac6498372a64a54f7dac25f4ead7a
Yulai202020 <ngmyulai@gmail.com>
2023-08-04 13:15:59 +0500

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import uvicorn
 from router import router
 from fastapi import FastAPI
 
 app = FastAPI()
 app.include_router(router = router)
-
-# run by uvicorn
-def run(host: str, port: int):
-    uvicorn.run(app, host = host, port = port)
-
-if __name__ == "__main__" :
-    run(host = "0.0.0.0", port = 8000)
